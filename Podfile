8ba114a568c4162f4e889005855f5451934d0017
Nicolai Henriksen <nih@miracle.dk>
2016-09-08 14:11:59 +0100

    Added Moscapsule to an otherwise empty project

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+
+use_frameworks!
+
+target 'ThreeDinosaurs' do
+    pod 'Moscapsule', :git => 'https://github.com/flightonary/Moscapsule.git'
+    pod 'OpenSSL-Universal', '~> 1.0.1.18'    
+end
---
+++ ThreeDinosaurs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThreeDinosaurs
+//
+//  Created by Nicolai Henriksen on 08/09/16.
+//  Copyright © 2016 ThreeDinosaurs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
